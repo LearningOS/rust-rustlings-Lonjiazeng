f87c70c113593e383fbdde4b2b76e14698d59097
LJZ <jz.long@qq.com>
2023-03-25 13:50:49 +0800

    update

--- exercises/intro/intro1.rs
+++ exercises/intro/intro1.rs
@@ -9,8 +9,6 @@
 // when you change one of the lines below! Try adding a `println!` line, or try changing
 // what it outputs in your terminal. Try removing a semicolon and see what happens!
 
-// I AM NOT DONE
-
 fn main() {
     println!("Hello and");
     println!(r#"       welcome to...                      "#);
@@ -26,5 +24,12 @@ fn main() {
     println!("solve the exercises. Good luck!");
     println!();
     println!("The source for this exercise is in `exercises/intro/intro1.rs`. Have a look!");
-    println!("Going forward, the source of the exercises will always be in the success/failure output.");
+    println!(
+        "Going forward, the source of the exercises will always be in the success/failure output."
+    );
+    println!();
+    println!(
+        "If you want to use rust-analyzer, Rust's LSP implementation, make sure your editor is set"
+    );
+    println!("up, and then run `rustlings lsp` before continuing.")
 }
--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,14 +2,13 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
-
 fn main() {
     let data = "Rust is great!".to_string();
 
-    get_char(data);
+    get_char(data.clone());
 
     string_uppercase(&data);
+    println!("{}", data);
 }
 
 // Should not take ownership
@@ -19,7 +18,7 @@ fn get_char(data: String) -> char {
 
 // Should take ownership
 fn string_uppercase(mut data: &String) {
-    data = &data.to_uppercase();
+    let data = data.to_uppercase();
 
     println!("{}", data);
 }
